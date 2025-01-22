bce598e6d860dd770ce778b9ff29bcfec9240955
Norberto Boa <nboa.26@gmail.com>
2025-01-22 16:00:09 +0200

    Fix: anchors tags function were not working
    - Anchor tags should close navigation post clicked
    - Anchor tags from buttons should redirect to each page section
    - Add mailto for anchor tags to ask for quotations

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,19 +2,30 @@ const burguer = document.getElementById("burguer");
 const closeMenu = document.getElementById("close-menu");
 const mobileNav = document.querySelector("#mobile-nav");
 const itemsContainer = document.querySelector("#items-container");
+const navigationLinks = document.querySelectorAll("#items-container a");
 
 burguer.addEventListener("click", () => {
   mobileNav.classList.toggle("display-nav");
   setTimeout(() => {
     itemsContainer.classList.toggle("display-nav-items");
-  }, 500)
+  }, 100)
 })
 
 closeMenu.addEventListener("click", () => {
   itemsContainer.classList.toggle("display-nav-items");
   setTimeout(() => {
     mobileNav.classList.toggle("display-nav");
-  }, 200)
+  }, 100)
+})
+
+
+navigationLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    itemsContainer.classList.remove("display-nav-items")
+    setTimeout(() => {
+      mobileNav.classList.remove("display-nav")
+    }, 200)
+  })
 })
 
 window.addEventListener("resize", () => {
